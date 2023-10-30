a94cd469a2ba055feeae1c592e0886847d598af8
Ishank-Coder <ishuishank18@gmail.com>
2023-10-31 00:44:51 +0530

    data_preprocessing

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,2 +1,14 @@
-def process():
-    return 0
+from icecream import ic
+def multilingual(a,translator):
+    try:
+        ic(100)
+        return(translator.translate(a, dest="en").text)
+        
+    except:
+        
+        return(a)
+
+def process(data,translator):
+    data['new'] = data['Tweet'].apply(lambda review: multilingual(review,translator))
+    return(data)
+
